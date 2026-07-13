ae1b06acdead9fa7fb7b376e0a0a5cc209cdd44b
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-07-13 13:51:46 +0800

    chore: reuse shared test utilities (#98)

--- test/helper.ts
+++ test/helper.ts
@@ -1,33 +1,6 @@
 import path from 'node:path';
+export { proxyConsole } from '@rstackjs/test-utils';
 import upath from 'upath';
-
-export const proxyConsole = (
-  types: string[] = ['log', 'warn', 'info', 'error'],
-) => {
-  const logs: string[] = [];
-  const restores: Array<() => void> = [];
-
-  for (const type of types) {
-    const method = console[type];
-
-    restores.push(() => {
-      console[type] = method;
-    });
-
-    console[type] = (log) => {
-      logs.push(log);
-    };
-  }
-
-  return {
-    logs,
-    restore: () => {
-      for (const restore of restores) {
-        restore();
-      }
-    },
-  };
-};
 
 export const normalizeToPosixPath = (p: string | undefined) =>
   upath
